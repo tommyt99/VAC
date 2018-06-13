1bc26020b0d7c8ec52120e9ec4455cf64eef29d3
Tommy Tran <tommyvtran36@gmail.com>
2018-06-13 11:30:43 -0500

    Practice code

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int numbers[100] = { 1,2,3 };
+	//int number[ 100 ] = {1,2,3, [10] = 10, 11, 12, [60] = 50, [42] = 420 };
+	numbers[10] = 21;
+	for (int i = 0; i <= 100; ++i) {
+		cout << numbers[i];
+	}
+
+	int o;
+
+	cin >>  o; 
+	return 0;
+}
+
