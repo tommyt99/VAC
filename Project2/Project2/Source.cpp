b7750bfb8a197fe31deec0a39c5e0c8c903fac10
Tommy Tran <tommyvtran36@gmail.com>
2018-06-15 10:12:45 -0500

    Initial commit

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-	int numbers[100] = { 1,2,3 };
+	int numbers[20] = { 1,2,3 };
 	//int number[ 100 ] = {1,2,3, [10] = 10, 11, 12, [60] = 50, [42] = 420 };
 	numbers[10] = 21;
-	for (int i = 0; i <= 100; ++i) {
+	for (int i = 0; i < 20; ++i) {
 		cout << numbers[i];
 	}
 
