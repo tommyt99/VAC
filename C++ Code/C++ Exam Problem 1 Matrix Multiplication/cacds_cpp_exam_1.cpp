9ddef52a18f1490337a772dbeb6d4a6ca828417e
Tommy Tran <tommyvtran36@gmail.com>
2018-07-05 10:11:02 -0500

    July 5, 2018
    Block Matrix Multiplication

--- C++ Code/C++ Exam Problem 1 Matrix Multiplication/cacds_cpp_exam_1.cpp
+++ C++ Code/C++ Exam Problem 1 Matrix Multiplication/cacds_cpp_exam_1.cpp
@@ -1,4 +1,5 @@
-// Matrix Multiplication
+// Matrix Multiplication 
+/*
 #include <iostream>
 using namespace std;
 
@@ -70,36 +71,44 @@ int main()
 }
 
 
+*/
 
 
+//Block multiplication
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main()
-{
-	int  r1, c1, r2, c2, i, j, k;
-
-	cout << "Enter rows and columns for first matrix: ";
+{ // nxn matrix, sxs blocks 
+	//int  r1, c1, r2, c2;
+		int i, j, k;
+	int s = 1; //Block size
+	const int n = 2; 
+	int N = n/s; // n/s
+	double temp=0; 
+	time_t tstart, tend; 
+	/*cout << "Enter rows and columns for first matrix: ";
 	cin >> r1 >> c1;
 	cout << "Enter rows and columns for second matrix: ";
-	cin >> r2 >> c2;
+	cin >> r2 >> c2;*/
 
-	int a[r1][c1], b[r2][c2], mult[r1][c2];
+	int a[n][n], b[n][n], mult[n][n];
 
 	// If column of first matrix in not equal to row of second matrix,
 	// ask the user to enter the size of matrix again.
-	while (c1 != r2)
+	/*while (c1 != r2)
 	{
 		cout << "Enter rows and columns for first matrix: ";
 		cin >> r1 >> c1;
 		cout << "Enter rows and columns for second matrix: ";
 		cin >> r2 >> c2;
-	}
+	}*/
 
 	// Storing elements of first matrix.
 	cout << endl << "Enter elements of matrix 1:" << endl;
-	for (i = 0; i < r1; ++i)
-		for (j = 0; j < c1; ++j)
+	for (i = 0; i < n; ++i)
+		for (j = 0; j < n; ++j)
 		{
 			cout << "Enter element a" << i + 1 << j + 1 << " : ";
 			cin >> a[i][j];
@@ -107,20 +116,21 @@ int main()
 
 	// Storing elements of second matrix.
 	cout << endl << "Enter elements of matrix 2:" << endl;
-	for (i = 0; i < r2; ++i)
-		for (j = 0; j < c2; ++j)
+	for (i = 0; i < n; ++i)
+		for (j = 0; j < n; ++j)
 		{
 			cout << "Enter element b" << i + 1 << j + 1 << " : ";
 			cin >> b[i][j];
 		}
 
 	// Initializing elements of matrix mult to 0.
-	for (i = 0; i < r1; ++i)
-		for (j = 0; j < c2; ++j)
+	for (i = 0; i < n; ++i)
+		for (j = 0; j < n; ++j)
 		{
 			mult[i][j] = 0;
 		}
 
+	tstart = time(0);
 	// Block Multiplication
 	for (int jj = 0; jj<N; jj += s) {
 		for (int kk = 0; kk<N; kk += s) {
@@ -135,14 +145,16 @@ int main()
 			}
 		}
 	}
+	tend = time(0);
+	cout << "It took " << difftime(tend, tstart) << " second(s)." << endl;
 
 	// Displaying the multiplication of two matrix.
 	cout << endl << "Output Matrix: " << endl;
-	for (i = 0; i < r1; ++i)
-		for (j = 0; j < c2; ++j)
+	for (i = 0; i < n; ++i)
+		for (j = 0; j < n; ++j)
 		{
 			cout << " " << mult[i][j];
-			if (j == c2 - 1)
+			if (j == n - 1)
 				cout << endl;
 		}
 	system("PAUSE");
